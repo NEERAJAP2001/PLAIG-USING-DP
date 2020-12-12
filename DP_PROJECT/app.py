194ad9ea700e147b2ebd9b44261e727e71a691b0
NEERAJ ADITYANANTH POLAMPALLI <65017645+NEERAJAP2001@users.noreply.github.com>
2020-12-12 10:47:20 +0530

    Update app.py

--- DP_PROJECT/app.py
+++ DP_PROJECT/app.py
@@ -7,20 +7,20 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-	return render_template('index.html')
+	return render_template('index.html')                                    ## return index page intially
 
 @app.route('/predict', methods=['POST'])
 def predict():
 	if request.method == 'POST':
 		X = request.form['para1']
-		Y = request.form['para2']
+		Y = request.form['para2']                                       ## take in paras from html page.
 		if(len(X)==0 or len(Y)==0):
-			return render_template('index.html',percent=0)
+			return render_template('index.html',percent=0)          ## as nothing is common we give 0 as a parameter
 		else:
-			a = lcs(X,Y)
-			b = a/max(len(X),len(Y))*100
+			a = lcs(X,Y)                                            ## finding LCS b/w them
+			b = a/max(len(X),len(Y))*100                            ## using logic for finding percentage
 			percent = str(round(b, 2))
-			return render_template('index.html',percent=percent)
+			return render_template('index.html',percent=percent)    ## returning percentage to the page back 
 
 if __name__ == '__main__':
 	app.run(debug=True)
