c4e7cb70ce14d2c19f13725d30ccd12f36fbce27
NEERAJ ADITYANANTH POLAMPALLI <65017645+NEERAJAP2001@users.noreply.github.com>
2020-12-28 11:34:33 +0530

    Update back.py

--- DP_PROJECT/back.py
+++ DP_PROJECT/back.py
@@ -1,18 +1,19 @@
 
 def lcs(X,Y): 
     ## Finding LCS Function starts here 
-    m = len(X)                                        ## find the length of both paragraphs
-    n = len(Y) 
-    L = [[None]*(n+1) for i in range(m+1)]            ## using list compreshension for finding 'L'
-    for i in range(m+1): 
-        for j in range(n+1):                          ## loop over the paragraphs length
+    lenght_X = len(X)                                                              ## find the length of both paragraphs as len(X),len(Y)
+    lenght_Y= len(Y) 
+    List_res= [[None]*(lenght_Y+1) for i in range(lenght_X+1)]                     ## using list compreshension for finding 'List_res'
+    
+    for i in range(length_X+1): 
+        for j in range(lenght_Y+1):                                                ## loop over the paragraphs length
             if i == 0 or j == 0 : 
-                L[i][j] = 0                           ## as nothing is common b/w them
+                List_res[i][j] = 0                                                 ## as nothing is common b/w them
             elif X[i-1] == Y[j-1]: 
-                L[i][j] = L[i-1][j-1]+1               ## as we have common elements
+                List_res[i][j] = List_res[i-1][j-1]+1                              ## as we have common elements
             else: 
-                L[i][j] = max(L[i-1][j] , L[i][j-1])  ## as we want longest so max()
-    return L[m][n] 
+                List_res[i][j] = max(List_res[i-1][j] , List_res[i][j-1])          ## as we want longest so max() is being used 
+    return List_res[length_X][length_N] 
     ## returns the result
 
   
